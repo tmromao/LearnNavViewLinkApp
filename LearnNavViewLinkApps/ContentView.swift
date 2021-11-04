826bf992e587ea6d3d06396cd161396af8dadb2a
tromao <tiagomromao@gmail.com>
2021-11-04 21:43:22 +0000

    Initial Commit

---
+++ LearnNavViewLinkApps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnNavViewLinkApps
+//
+//  Created by Tiago Miguel de Jesus Romao on 04/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnNavViewLinkApps/LearnNavViewLinkAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnNavViewLinkAppsApp.swift
+//  LearnNavViewLinkApps
+//
+//  Created by Tiago Miguel de Jesus Romao on 04/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearnNavViewLinkAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
