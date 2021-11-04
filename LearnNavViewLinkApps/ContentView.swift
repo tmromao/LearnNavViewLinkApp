edd4d8a106dd1725e4e7ffb277e529d391271ad0
tromao <tiagomromao@gmail.com>
2021-11-04 23:34:01 +0000

    testing navview and navlink unwind navigation

--- LearnNavViewLinkApps/ContentView.swift
+++ LearnNavViewLinkApps/ContentView.swift
@@ -8,9 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isActive:Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            NavigationLink(isActive: $isActive){
+                SecondView(isActive: $isActive)
+            } label: {
+                Text("Second view")
+            }.navigationTitle("Content view")
+            
+            
+//            NavigationLink {
+//                SecondView()
+//            }label: {
+//                Text("Go to Second view")
+//            }.navigationTitle("Content view")
+//
+            //        List(1...20, id:\.self){index in
+            //            NavigationLink {
+            //                DetailView(index: index)
+            //            } label: {
+            //                Text("\(index)")
+            //            }
+            //
+            //
+            //        }
+            //            NavigationLink(isActive: $isActive) {
+            //                DetailView()
+            //            } label: {
+            //                Button("Go to detail view"){
+            //                    isActive = true
+            //                }
+            //            }
+            //
+            
+        }
+        
+    }
+    
+}
+
+struct SecondView: View{
+    
+    @Binding var isActive: Bool
+    
+    var body: some View {
+        NavigationLink {
+            ThirdView(isActive: $isActive)
+        } label: {
+            Text("Third view")
+        }.navigationTitle("Second view")
+    }
+}
+
+struct ThirdView:View{
+    
+    @Binding var isActive: Bool
+    
+    var body: some View {
+        NavigationLink {
+            ContentView()
+        } label: {
+            Button("Go to content view")	{
+                isActive = false
+            }
+        }.navigationTitle("Third view")
+    }
+}
+
+
+struct DetailView:View{
+    
+    let index:Int
+    
+    var body: some View {
+        Text("Detail view : \(index)")
     }
 }
 
@@ -19,3 +95,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
